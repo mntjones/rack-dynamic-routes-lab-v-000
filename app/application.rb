648b26c21adb68c1c88596d3d07fad79e3b6f0ac
Monica Tamaru Jones <mntamaru@gmail.com>
2018-04-16 17:57:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,32 @@
 require 'pry'
 
 class Application
+<<<<<<< HEAD
   
   @@items = []
+=======
+>>>>>>> a08015e5bfcdb3662c5bb900586988c9c52409d1
   def call (env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
+<<<<<<< HEAD
     if req.path.include? ("/items/")
       
       item_req = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_req}
 
       if item
+=======
+    @@item = []
+    
+    if req.path.include? ("/items/")
+      
+      item_req = req.path.split("/items/").last
+      binding.pry
+      if @@item.include? (item_req)
+        item = @@item.find {|i| i.name == item_req}
+>>>>>>> a08015e5bfcdb3662c5bb900586988c9c52409d1
         resp.write "#{item.price}"
         resp.status = 200
       else
